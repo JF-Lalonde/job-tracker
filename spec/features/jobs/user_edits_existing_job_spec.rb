a7bca5853f50485c3099522d19137c8243d9cfb6
JF-Lalonde <lalondej88@gmail.com>
2017-07-13 13:40:26 -0600

    add edit functionality to jobs

--- spec/features/jobs/user_edits_existing_job_spec.rb
+++ spec/features/jobs/user_edits_existing_job_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 describe "User edits an existing job" do
   scenario "a user can edit a job" do
+    company = Company.create!(name: "ESPN")
+    job = Job.create!(title: "Assistant", level_of_interest: 70, city: "Tacoma", company_id: company.id)
 
+    visit companies_path
+    click_on "ESPN"
+    click_on "Assistant"
+    click_on "Edit Job"
+    fill_in "job[title]", with: "Developer"
+    fill_in "job[description]", with: "So fun!"
+    fill_in "job[level_of_interest]", with: 80
+    fill_in "job[city]", with: "Denver"
+
+
+    click_button "Update"
+
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
+    expect(page).to have_content(company.name)
+    expect(page).to have_content("Developer")
+    expect(page).to_not have_content("Assistant")
+    expect(page).to have_content("80")
+    expect(page).to have_content("Denver")
   end
 end
---
+++ spec/support/jobs.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :job do
+    title "Assistant to the Dev"
+    description "Mostly getting coffee"
+    level_of_interest "99"
+    city "Fargo"
+    associations :company
+  end
+
+  factory :company do
+    sequence :name do |n|
+      "#{n} Goodwill"
+    end
+  end
+end
