68bd3982cc43b192045d15a1fc10625e95bbbc17
JF-Lalonde <lalondej88@gmail.com>
2017-07-18 11:34:11 -0600

    fix delete route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    user = User.find_by_username(params[:username]) # If using form_for, params will be nested in params[:session][:username], etc. find_by_username is deprecated, better to use User.find_by(username: username)
     if user && user.authenticate(params[:password])
       flash[:message] = "Logged in successfully!"
-      session[:user_id] = user.id
+      session[:user_id] = user.id #Very important: this is where a session will have info about a user
       redirect_to 'companies#index'
     else
       flash[:error] = "Invalid Login!"
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     flash[:message] = "Logged Out!"
-    session[:user_id] = nil
+    session[:user_id] = nil #Could use session.clear instead, would clear whole session info
     redirect_to '/login'
   end
 end
